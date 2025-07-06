2f4d4c7f39cb10c43263679e93a0f45f897a5335
George Kornukh <egor@kornukh.ru>
2025-07-06 22:17:09 +0300

    HW5 is completed

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,42 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("tasks without errors without using time.Sleep", func(t *testing.T) {
+		const workersCount = 5
+		tasks := make([]Task, 50)
+		var runTasksCount int32
+		block := make(chan struct{})
+		for i := range tasks {
+			tasks[i] = func() error {
+				atomic.AddInt32(&runTasksCount, 1)
+				<-block
+				return nil
+			}
+		}
+		go Run(tasks, workersCount, 0)
+		require.Eventually(t, func() bool {
+			return atomic.LoadInt32(&runTasksCount) == workersCount
+		}, time.Second, 10*time.Millisecond)
+		close(block)
+	})
+
+	t.Run("ignore errors mode", func(t *testing.T) {
+		tasksCount := 50
+		var runTasksCount int32
+		tasks := make([]Task, tasksCount)
+		for i := range tasks {
+			err := fmt.Errorf("error from task %d", i)
+			tasks[i] = func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			}
+		}
+		workersCount := 10
+		maxErrorsCount := 0
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+		require.Nil(t, err, "non-nil error is returned")
+	})
 }
